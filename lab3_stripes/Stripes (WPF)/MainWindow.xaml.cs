b2d7e27a1beec07076a9555469a14cc53de29771
Vladislav Klimenko <vlad.klimenko.mmv@bk.ru>
2025-04-25 17:32:21 +0300

    upload the third lab work

---
+++ lab3_stripes/Stripes (WPF)/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Stripes
+{
+    public partial class MainWindow : Window
+    {
+        private List<Tuple<Rect, SolidColorBrush>> stripes = new List<Tuple<Rect, SolidColorBrush>>();
+        private int stripesCountToWin = 5;
+        private int maxStripesCount = 2 * 5; 
+        private int clickedCounter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1) 
+            };
+            timer.Tick += (sender, e) =>
+            {
+                if (stripes.Count < maxStripesCount)
+                {
+                    Random random = new Random();
+                    int width = random.Next(50, 150);
+                    int height = random.Next(50, 150);
+                    double x = random.Next(0, (int)canvas.ActualWidth + width);
+                    double y = random.Next(0, (int)canvas.ActualHeight + height);
+
+                    Rect newStripe;
+                    SolidColorBrush fillColor = new SolidColorBrush(Color.FromRgb((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)));
+
+                    if (random.Next(2) == 0) 
+                    {
+                        newStripe = new Rect(x, y, width, height);
+                    }
+                    else
+                    {
+                        newStripe = new Rect(x, y, height, width); 
+                    }
+
+                    stripes.Add(new Tuple<Rect, SolidColorBrush>(newStripe, fillColor));
+
+                    DrawStripe(newStripe, fillColor);
+                }
+                else
+                {
+                    timer.Stop();
+                    MessageBox.Show("Game over! You lose.");
+                }
+            };
+            timer.Start();
+        }
+
+        private void DrawStripe(Rect rect, SolidColorBrush color)
+        {
+            Rectangle rectangle = new Rectangle
+            {
+                Width = rect.Width,
+                Height = rect.Height,
+                Fill = color
+            };
+            Canvas.SetLeft(rectangle, rect.Left);
+            Canvas.SetTop(rectangle, rect.Top);
+            canvas.Children.Add(rectangle);
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            for (int i = stripes.Count - 1; i >= 0; i--)
+            {
+                if (stripes[i].Item1.Contains(e.GetPosition(canvas)))
+                {
+                    bool canRemove = true;
+                    foreach (Tuple<Rect, SolidColorBrush> otherStripe in stripes)
+                    {
+                        if (stripes[i].Item1 != otherStripe.Item1 && stripes[i].Item1.IntersectsWith(otherStripe.Item1))
+                        {
+                            canRemove = false;
+                            break;
+                        }
+                    }
+                    if (canRemove)
+                    {
+                        DispatcherTimer t = new DispatcherTimer
+                        {
+                            Interval = TimeSpan.FromSeconds(1) 
+                        };
+
+                        stripes.RemoveAt(i);
+                        clickedCounter++;
+                        canvas.Children.Clear();
+
+                        foreach (Tuple<Rect, SolidColorBrush> stripe in stripes)
+                        {
+                            DrawStripe(stripe.Item1, stripe.Item2);
+                        }
+
+                        if (stripes.Count == 0) {
+                            t.Tick += delegate { Close(); t.Stop(); };
+                            t.Start();
+                            MessageBox.Show("Congratulations! You win!");
+                        } 
+
+                        else if (clickedCounter == stripesCountToWin) {
+                            t.Tick += delegate { Close(); t.Stop(); };
+                            t.Start();
+                            MessageBox.Show("Congratulations! You clicked required amount of lines!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab3_stripes/Stripes (Windows Forms)/Stripes/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        private List<Tuple<Rectangle, Color>> stripes = new List<Tuple<Rectangle, Color>>();
+        private int stripesCountToWin = 5; 
+        private int maxStripesCount = 2 * 5;
+        private int clickedCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+            this.MouseClick += Form1_MouseClick;
+            this.Size = new Size(1280, 720);
+
+            Timer timer = new Timer
+            {
+                Interval = 1000 
+            };
+
+            timer.Tick += (sender, e) =>
+            {
+                if (stripes.Count < maxStripesCount)
+                {
+                    Random random = new Random();
+                    int width = 150;
+                    int height = 20;
+                    int x = random.Next(0, this.Width - width);
+                    int y = random.Next(0, this.Height - height);
+                    Rectangle newStripe = new Rectangle(x, y, width, height);
+                    Color fillColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+                    if (random.Next(2) == 0) 
+                    {
+                        newStripe = new Rectangle(x, y, width, height);
+                    }
+                    else
+                    {
+                        newStripe = new Rectangle(x, y, height, width);
+                    }
+
+                    stripes.Add(new Tuple<Rectangle, Color>(newStripe, fillColor));
+
+                    this.Invalidate();
+                }
+                else
+                {
+                    timer.Stop();
+                    MessageBox.Show("Game over! You lost.");
+                }
+            };
+            timer.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Tuple<Rectangle, Color> stripe in stripes)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(stripe.Item2), stripe.Item1);
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            for (int i = stripes.Count - 1; i >= 0; i--)
+            {
+                if (stripes[i].Item1.Contains(e.Location))
+                {
+                    bool canRemove = true;
+                    foreach (Tuple<Rectangle, Color> otherStripe in stripes)
+                    {
+                        if (stripes[i].Item1 != otherStripe.Item1 && stripes[i].Item1.IntersectsWith(otherStripe.Item1))
+                        {
+                            canRemove = false;
+                            break;
+                        }
+                    }
+                    if (canRemove)
+                    {
+                        Timer t = new Timer
+                        {
+                            Interval = 1000
+                        };
+                        stripes.RemoveAt(i);
+                        clickedCounter++;
+                        this.Invalidate();
+                        if (clickedCounter == stripesCountToWin)
+                        {
+                            t.Tick += delegate { Close(); t.Stop(); };
+                            t.Start();
+                            MessageBox.Show("Congratulations! You won!");
+                        
+                        } else if (stripes.Count == 0)
+                        {
+                            t.Tick += delegate { Close(); t.Stop(); };
+                            t.Start();
+                            MessageBox.Show("Congratulations! You clicked all lines and won!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
