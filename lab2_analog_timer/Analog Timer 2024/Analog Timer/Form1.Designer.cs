4bfad9162710fc7efe462e9eb52e3956be2f124d
Vladislav Klimenko <vlad.klimenko.mmv@bk.ru>
2025-04-26 18:13:36 +0300

    upload the second lab work

---
+++ lab2_analog_timer/Analog Timer 2024/Analog Timer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AnalogTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Часы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Минуты";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Секунды";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(693, 30);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 214);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Analog Timer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ lab2_analog_timer/Analog Timer 2024/Analog Timer/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AnalogTimer
+{
+    public partial class Form1 : Form
+    {
+        private AnalogClockTimer hourClock;
+        private AnalogClockTimer minuteClock;
+        private AnalogClockTimer secondClock;
+        private Button startButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            hourClock = new AnalogClockTimer(40, 40, 100, 100, 0, 24 * 60 * 60 );
+            minuteClock = new AnalogClockTimer(160, 40, 100, 100, 0, 60 * 60);
+            secondClock = new AnalogClockTimer(280, 40, 100, 100, 0, 60 );
+
+            startButton = new Button();
+            startButton.Text = "Старт";
+            startButton.Location = new Point(400, 90);
+            startButton.Click += StartButton_Click;
+
+            Controls.Add(hourClock);
+            Controls.Add(minuteClock);
+            Controls.Add(secondClock);
+            Controls.Add(startButton);
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            hourClock.Start();
+            minuteClock.Start();
+            secondClock.Start();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+
+    public class AnalogClockTimer : Control
+    {
+        private int minValue;
+        private int maxValue;
+        private int value;
+        private bool isDragging;
+        private Point mouseStart;
+
+        public AnalogClockTimer(int x, int y, int width, int height, int minValue, int maxValue)
+        {
+            Location = new Point(x, y);
+            Size = new Size(width, height);
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            value = minValue;
+
+            MouseDown += AnalogClockTimer_MouseDown;
+            MouseMove += AnalogClockTimer_MouseMove;
+            MouseUp += AnalogClockTimer_MouseUp;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            g.FillEllipse(Brushes.White, ClientRectangle);
+            g.DrawEllipse(Pens.Black, ClientRectangle);
+
+            double angle = (value - minValue) / (double)(maxValue - minValue) * 360 * Math.PI / 180;
+            int x = ClientSize.Width / 2 + (int)(ClientSize.Width / 2 * Math.Sin(angle));
+            int y = ClientSize.Height / 2 - (int)(ClientSize.Height / 2 * Math.Cos(angle));
+
+            g.DrawLine(Pens.Black, ClientSize.Width / 2, ClientSize.Height / 2, x, y);
+        }
+
+        private void AnalogClockTimer_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && ClientRectangle.Contains(e.Location))
+            {
+                isDragging = true;
+                mouseStart = e.Location;
+            }
+        }
+
+        private void AnalogClockTimer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                double angle = Math.Atan2(e.Y - ClientSize.Height / 2, e.X - ClientSize.Width / 2);
+                value = (int)Math.Round(angle * 180 / Math.PI * (maxValue - minValue) / 360 + minValue);
+                if (value < minValue) value = minValue;
+                if (value > maxValue) value = maxValue;
+                Invalidate();
+            }
+        }
+
+        private void AnalogClockTimer_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        public void Start()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            value++;
+            if (value > maxValue)
+                value = minValue;
+            Invalidate();
+        }
+    }
+}
